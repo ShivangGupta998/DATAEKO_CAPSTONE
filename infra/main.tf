add3a33ab83d314da5aa28fc1016cad49d6855b0
Shivang Gupta <shivanggupta998@gmail.com>
2026-09-16 13:14:24 +0530

    fix(infra): restrict SSH ingress to internal VPC CIDR

--- infra/main.tf
+++ infra/main.tf
@@ -47,12 +47,11 @@ resource "aws_security_group" "api" {
   name        = "${var.student}-capstone-api"
   description = "capstone api"
 
-  # DEFECT: the whole internet can reach SSH on this box.
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   ingress {
